2208e6201dd79c43c67a0d0b84d0901f03a5784a
Scott Sirowy <ssirowy@gmail.com>
2018-05-09 14:43:55 -0700

    Changing names from bots to oracles.

--- src/bots.js
+++
@@ -1,6 +0,0 @@
-import BasicBot from './bots/base-bot';
-
-// Reference your bot class here using an import statment like above.
-
-// Add a new instance of your into here for it to be picked by the app.
-export default [ new BasicBot() ];
---
+++ src/oracles.js
@@ -0,0 +1,6 @@
+import RandomOracle from './oracles/random-oracle';
+
+// Reference your oracle class here using an import statment like above.
+
+// Add a new instance of your oracle here. 
+export default [ new RandomOracle() ];
