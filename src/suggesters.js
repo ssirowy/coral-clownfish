f651728681a4ee6bdd0a44843d9891554a208e6a
Scott Sirowy <ssirowy@gmail.com>
2018-05-09 15:53:48 -0700

    Changed everything to suggesters.

---
+++ src/suggesters.js
@@ -0,0 +1,6 @@
+import RandomSuggester from './suggesters/random';
+
+// Reference your suggester class here using an import statment like above.
+
+// Add a new instance of your suggester here. 
+export default [ new RandomSuggester('Random') ];
---
+++ src/suggesters/suggester.js
@@ -0,0 +1,82 @@
+/**
+   This defines the basic interface of a suggester. You should override this class and then write
+   your own suggestion algorithm.
+*/
+export default class Suggester {
+
+    /**
+       Suggester constructor.
+       @method constructor
+       @param {String}
+       @return {void}
+    */
+    constructor(name) {
+        this._name = name;
+    }
+
+    /**
+       Returns name of suggester.
+       @method getTitle
+       @return {String}
+    */
+    getName() {
+        return this._name;
+    }
+
+    /**
+       This is the meat of the suggester. Given a board state, suggest the next cell to click
+       on the board.
+       @method nextCellClicksuggestion
+       @param {Object} A 2D array of the board. Remember the actual board is 1-indexed (the 0 indices are the constraints for the board)
+       @return {Cell}
+    */
+    nextSuggestion(board) {
+        throw 'Unimplemented';
+    }
+
+    /**
+
+    start(store) {
+
+        this.store = store;
+        this.unsubscribe = store.subscribe(state => {
+            this.executeStep(store.getState().game.board);
+        });
+
+        store.dispatch(setBotRunning(true));
+    }
+
+    stop() {
+        this.unsubscribe();
+        this.store.dispatch(setBotRunning(false));
+
+        this.i = 1;
+        this.unsubscribe = this.store = null;
+    }
+
+    executeStep(board) {
+        console.log(board);
+
+        if (this.i < 7) {
+            this.clickCell(2, this.i);
+            this.i++;
+        }
+        else {
+            this.stop();
+        }
+    }
+
+    _sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
+    async clickCell(row, column) {
+        if (this.sleepMS) {
+            await this._sleep(this.sleepMS);
+        }
+
+        this.store.dispatch(clickCell(row, column));
+    }
+
+    */
+}
