6317aa937e316bcd20501b36a9c607a4711f0a7b
Scott Sirowy <ssirowy@gmail.com>
2018-05-10 08:27:20 -0700

    Creating a README.org

--- src/utils/predefined-games.js
+++ src/utils/predefined-games.js
@@ -127,7 +127,7 @@ function _generateGame(config) {
             }
 
             let type = abbreviatedTypes[board[i - 1][j - 1]];
-            const debug = true;
+            const debug = false;
 
             if (!debug) {
                 type = (type === 'clownfish') ? 'empty' : type;
